9bd76ffdbe1fd96435e2c971be4ff77e046fdd65
Gulesh <gshukla@haverford.edu>
2020-04-15 14:59:14 -0400

    Initial Commit

---
+++ MovieFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFinder
+//
+//  Created by Gulesh Shukla on 4/15/20.
+//  Copyright © 2020 Gulesh Shukla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
